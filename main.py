bf8ff6402e45f3386b9c9c272b783b89e13a4656
Saksham Sharma <sakshamsharma03221@gmail.com>
2026-02-10 16:10:01 +0530

    FINAL CODE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import hashlib as h
 from colorama import Fore, Style, Back
-def hash():
+def hashSHA512():
     target = input("enter hashed password:")
     f1 = open("input.txt" , "r")
     text = f1.readlines()
@@ -9,5 +9,46 @@ def hash():
         hashed = h.sha512(password.encode()).hexdigest()
         if hashed == target:
             print("Hash Decrypted:", hashed , "\n input password:", password)
+def hashSHA256():
+    target = input("enter hashed password:")
+    f1 = open("input.txt" , "r")
+    text = f1.readlines()
+    for i in range(len(text)):
+        password = text[i].strip("\n")
+        hashed = h.sha256(password.encode()).hexdigest()
+        if hashed == target:
+            print("Hash Decrypted:", hashed , "\n input password:", password)
+def hashMD5():
+    target = input("enter hashed password:")
+    f1 = open("input.txt" , "r")
+    text = f1.readlines()
+    for i in range(len(text)):
+        password = text[i].strip("\n")
+        hashed = h.md5(password.encode()).hexdigest()
+        if hashed == target:
+            print("Hash Decrypted:", hashed , "\n input password:", password)
+def hashSHA1():
+    target = input("enter hashed password:")
+    f1 = open("input.txt" , "r")
+    text = f1.readlines()
+    for i in range(len(text)):
+        password = text[i].strip("\n")
+        hashed = h.sha1(password.encode()).hexdigest()
+        if hashed == target:
+            print("Hash Decrypted:", hashed , "\n input password:", password)
 
-hash()
+def menu():
+    print(Fore.RED + Style.BRIGHT + "CHOOSE ALGORITHM TO DECRYPT HASH:")
+    print(Fore.CYAN +Style.BRIGHT+ "[1.] SHA256") 
+    print(Fore.MAGENTA +Style.BRIGHT+ "[2.] SHA512")
+    print(Fore.CYAN +Style.BRIGHT+ "[3.] MD5") 
+    print(Fore.MAGENTA + Style.BRIGHT+ "[4.] SHA1")
+    ch = int(input(Fore.RED+Style.BRIGHT+"ENTER INTEGER CHOICE:")) 
+    if ch == 1:
+        hashSHA256()
+    if ch == 2:
+        hashSHA512()
+    if ch == 3:
+        hashMD5()
+    if ch == 4:
+        hashSHA1()
